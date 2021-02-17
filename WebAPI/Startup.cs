5689521cbd084c4bd611e1c0a996b1894ce744ff
fatmanur94 <fkumsal06@gmail.com>
2021-02-18 00:15:00 +0300

    17 Şubat

--- WebAPI/Startup.cs
+++ WebAPI/Startup.cs
@@ -31,8 +31,8 @@ namespace WebAPI
         {
             services.AddControllers();
             //Arkaplanda referans oluþturur. Ýçinde data tutmuyorsan kullan
-            services.AddSingleton<IProductService,ProductManager>();
-            services.AddSingleton<IProductDal, EfProductDal>();
+            //services.AddSingleton<IProductService,ProductManager>();
+            //services.AddSingleton<IProductDal, EfProductDal>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
