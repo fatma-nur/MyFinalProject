eaa103cac4e82d0d4abfbaf84a2c4c78a37b8bb1
fatmanur94 <fkumsal06@gmail.com>
2021-02-28 16:46:09 +0300

    28.02.2021

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -1,6 +1,8 @@
-﻿using Entities.Concrete;
+﻿using Core.Entities.Concrete;
+using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace Business.Constants
@@ -16,5 +18,12 @@ namespace Business.Constants
         public static string ProductCountOfCategoryError = "Bir kategoride en fazla 10 ürün olabilir.";
         public static string ProductNameAlreadyExists = "Aynı isimde ürün eklenemez.";
         public static string CategoryLimitExceeded = "Kategori limiti aşıldığı için yeni ürün eklenemez.";
+        public static string AuthorizationDenied = "Yetkiniz yok.";
+        public static string UserRegistered = "Kayıt oldu.";
+        public static string UserNotFound = "Kullanıcı bulunamadı.";
+        public static string PasswordError = "Hatalı parola";
+        public static string UserAlreadyExists = "Kullanıcı zaten kayıtlı.";
+        public static string SuccessfulLogin = "Giriş başarılı.";
+        public static string AccessTokenCreated = "Token oluşturuldu.";
     }
 }
